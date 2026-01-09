7ebab12109b4377446919535e9e633f0c12639ce
thounsova <thoun.sovannareach@institute.pse.ngo>
2026-01-09 12:56:44 +0700

    add anout

--- src/main/java/com/rental_api/rental/Dtos/Request/PropertyRequest.java
+++ src/main/java/com/rental_api/rental/Dtos/Request/PropertyRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
+import java.util.List;
 
 @Data
 public class PropertyRequest {
@@ -19,4 +20,7 @@ public class PropertyRequest {
     @NotNull(message = "Price is required")
     @Positive(message = "Price must be greater than 0")
     private Double price;
+
+    // ⚡ Add this field for images
+    private List<String> images;
 }
